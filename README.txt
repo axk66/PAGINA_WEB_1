5c6213c2b415009865f9e2d7cf32703bf9ab57c1
axk66 <ax.hernandez@duocuc.cl>
2023-05-08 17:15:51 -0400

    EV_PROGWEB_2

---
+++ README.txt
@@ -0,0 +1,3 @@
+Axel Hernández
+
+LINK REPOSITORIO: https://github.com/axk66/PAGINA_WEB_1
---
+++ js/github.js
@@ -0,0 +1,52 @@
+const searchGithub = async () => {
+    const username = document.getElementById("searchInput").value;
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const detailsContainer = document.querySelector(".details");
+    const data = await response.json();
+
+    if (response.ok) {
+        detailsContainer.style.display = "flex";
+        document.getElementById("result").innerHTML = `
+            <div class="profile">
+                <div class="profile-image">
+                    <img src="${data.avatar_url}" />
+                </div>
+                <div class="profile-details">
+                    <h2 class="name">${data.name || data.login}</h2>
+                    <p class="username">@${data.login}</p>
+                    <p class="bio">${data.bio || 'Esta cuenta no tiene biografia.'}</p>
+                    <div class="stats">
+                        <div>
+                            <div class="stats-name">Repositorios Publicos</div>
+                            <div class="stats-value">${data.public_repos}</div>
+                        </div>
+                        <div>
+                            <div class="stats-name">Seguidores</div>
+                            <div class="stats-value">${data.followers}</div>
+                        </div>
+                        <div>
+                            <div class="stats-name">Siguiendo</div>
+                            <div class="stats-value">${data.following}</div>
+                        </div>
+                    </div>
+                <div class="media">
+                    <p>
+                        <span class="media-value">${data.location || 'Ubicacion no disponible'}</span>
+                    </p>
+                    <p>
+                        <span class="media-value">${data.blog || 'Youtube no disponible'}</span>
+                    </p>
+                    <p>
+                        <span class="media-value">${data.twitter_username || 'Twitter no disponible'}</span>
+                    </p>
+                    <p>
+                        <span class="media-value">${data.company || 'Compania No disponible'}</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        `;
+    } else {
+        alert(data.message);
+    }
+}
